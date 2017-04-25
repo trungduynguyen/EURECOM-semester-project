2da332882d5c5abfaaebc854a2789d31101ea187
trungduynguyen <duy.nguyen2015@ict.jvn.edu.vn>
2017-04-25 09:00:34 +0200

    upload first data input sample

--- Crawl_recipe.py
+++ Crawl_recipe.py
@@ -5,61 +5,98 @@ Created on Thu Mar 16 12:51:45 2017
 @author: Trung Duy
 """
 
-from bs4 import BeautifulSoup
-
-#import requests
-
-
-#page = requests.get("http://www.food.com/recipe/all/popular?pn=2")
-
-#soup = BeautifulSoup(page.content, 'html.parser')
-
 import os
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from requests import get
-import datetime as tm
+import datetime as dtime
+import time
+from itertools import islice
+from lxml import etree as ET
+from unidecode import unidecode
+import random as rand
 
 
-
-
+alllink = []
 recipelinks = []
-
 inputfile = open('recipe_list.txt','r')
+#recipelinks = islice(inputfile,50)
+#for n in range(0,len(inputfile)):
+#    recipelinks.append(inputfile.readline())
+#with open('recipe_list.txt','r') as f:
+#    for n in range(1000,51000):
+#        recipelinks.append(f.readlines()[n])
+#for line in inputfile:
+alllink = inputfile.readlines()
 
-for line in inputfile:
-    line.strip('\n')
-    recipelinks.append(line)
-
-
-browser = webdriver.Chrome()
-browser.get(recipelinks[1])
-print(recipelinks[1])
-soup = BeautifulSoup(browser.page_source, 'html.parser')
-#print(soup.prettify())
-
-rate = str(soup.select('span.fd-rating-percent span')[0].getText())
-Num_of_vote = str(soup.select('a.af-show-reviews.af-review-count')[0].getText()).translate(None,"()")
-cook_time = str(soup.select('div.recipe-cooktime')[0].getText()).translate(None,"\n ").replace('READYIN:','')
-serving = str(soup.select('span.count')[0].getText())
-
-#list_ingredient = soup.select('ul.ingredient-list li')
-#for li in list_ingredient:
-#    print(li.find('span').getText() +'\t')
-#    print(li.getText())
+recipelinks = alllink[1000:51000]
 
 
+for i in range(0,len(recipelinks)/500):
 
-#list_direction = soup.select('ol.expanded li',)
-#for li in list_direction:
-#    print(li.getText()+'\n')
+    Dataset = ET.Element('Dataset')
+    k = 1
+    for link in recipelinks[i*500: (i+1)*500]:
+        link.strip('\n')
+        browser = webdriver.Chrome()
+        browser.get(link)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        
+        recipe_xml = ET.SubElement(Dataset,'recipe')
+        id_xml = ET.SubElement(recipe_xml, 'recipe_ID')
+        id_xml.text = str(unidecode(soup.select('button.rb-open.btn-emphasis')[0].get('data-recipe-id')))
+        
+        category_xml = ET.SubElement(recipe_xml, 'category')
+        category_xml.text = str(unidecode(soup.select('div.breadcrumbs.recipe-breadcrumbs.fd-container a')[2].getText()))
+        
+        link_xml = ET.SubElement(recipe_xml, 'link')
+        link_xml.text =  link
+        
+        name_xml = ET.SubElement(recipe_xml, 'recipe_name')
+        name_xml.text = str(unidecode(soup.select('header.recipe.fd-recipe h1')[0].getText()))
+        
+        rate_xml = ET.SubElement(recipe_xml, 'rate')
+        rate_xml.text = str(unidecode(soup.select('span.fd-rating-percent span')[0].getText()))
+        
+        vote_xml = ET.SubElement(recipe_xml, 'vote')
+        vote_xml.text = str(soup.select('a.af-show-reviews.af-review-count')[0].getText()).translate(None,"()")
+        
+        cook_time_xml = ET.SubElement(recipe_xml,'cook_time')
+        cook_time_xml.text = str(soup.select('div.recipe-cooktime')[0].getText()).translate(None,"\n ").replace('READYIN:','')
+        
+        serving_xml = ET.SubElement(recipe_xml,'serving')
+        serving_xml.text = str(soup.select('span.count')[0].getText())
+        
+        
+        list_ingredient = soup.select('ul.ingredient-list li')
+        list_ingredient_xml = ET.SubElement(recipe_xml,'list_ingredient') 
+        for li in list_ingredient:
+            ingredient = ET.SubElement(list_ingredient_xml,'ingredient')
+            ingredient.text = str(unidecode(li.getText()))
+        
+        
+        
+        list_direction = soup.select('ol.expanded li',)
+        list_direction_xml = ET.SubElement(recipe_xml,'list_direction') 
+        for li in list_direction[:-1]:
+            direction = ET.SubElement(list_direction_xml,'direction')
+            direction.text = str(unidecode(li.getText()))
+        
+        
+        print('Loading . . . . . . . . . ' + str(float((k*100)/500))+'%')
+        k+=1
+        browser.close()
+        browser.quit()
+    
+    
+    obj = ET.tostring(Dataset, pretty_print=True, xml_declaration=True)
+    output = open('./data/output_'+ str(i) +'.xml','w')
+    output.write(obj)
+    output.close()
+    
+    sleep_time = rand.randint(120, 300)
+    print('\n====================================\nFinnish output ' + str(i) + ' - Sleep time ' + str(sleep_time) + ' seconds\n====================================\n')
+    time.sleep(sleep_time)
 
-
-
-browser.quit()
-
-
-
-
-
+    
 
--- spider.py
+++ spider.py
@@ -6,8 +6,6 @@ Created on Wed Mar 08 16:29:35 2017
 """
 from bs4 import BeautifulSoup
 
-#import requests
-
 
 #page = requests.get("http://www.food.com/recipe/all/popular?pn=2")
 
@@ -18,48 +16,67 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from requests import get
 import datetime
+import time
+from selenium.webdriver.common.keys import Keys
+from lxml import etree as ET
+import random as rand
 
 
 
-#browser = webdriver.Chrome('C:\Users\Trung Duy\Documents\GitHub\EURECOM-semester-project\chromedriver.exe') # Instantiate a webdriver object
-browser = webdriver.Chrome()
-browser.get('http://www.food.com/recipe/all/popular?pn=1')
+#endTime = datetime.datetime.now() + datetime.timedelta(minutes=120)
+pre_link = 'http://www.food.com/services/mobile/fdc/search/sectionfront?pn='
+pos_link = '&searchTerm=&recordType=Recipe&sortBy=mostPopular&collectionId=17'
+link = []
+
+start_time = datetime.datetime.now()
+
+#total lenght 51250, split to 4 times
+for i in range(1,12813):
+    browser = webdriver.Chrome()
+#browser.get('http://www.food.com/recipe/all/popular?pn=1')
+#print(datetime.datetime.now())
+
+    browser.get(pre_link+str(i)+pos_link)
+
+#while True:
+#    if datetime.datetime.now() >= endTime:
+#        break
+#    #link = browser.find_element_by_css_selector('h2.title')
+#    browser.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+#    #browser.set_page_load_timeout(10)
+#    #browser.implicitly_wait(10)
+#    time.sleep(2)
+
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    browser.close()
+    browser.quit()
+
+    clean_html = soup.select('span.text')
+    record_url = []
+    for k in range(0,len(clean_html)):
+        record_url.append(clean_html[k].getText())
+
+    link = link + [str(s) for s in record_url if "http://www.food.com/recipe/" in s]
+    print('Done loop ' + str(i))
+    if (datetime.datetime.now().minute-start_time.minute)%9 == 1 :
+        time.sleep(120)
+    else:
+        time.sleep(rand.randint(1, 10))
 
 
-
-endTime = datetime.datetime.now() + datetime.timedelta(minutes=1)
-print(datetime.datetime.now())
-while True:
-    if datetime.datetime.now() >= endTime:
-        print(datetime.datetime.now())
-        break
-    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-soup = BeautifulSoup(browser.page_source, 'html.parser')
-#print(soup.prettify())
-#soup.find_all('h2', class_='title')
-# Makes list of links to get full image
-#links = []
-# This is the container of images on the main page
-#recipes = soup.find_all('h2', class_='title')
+#recipelinks = []
+#recipes = soup.select('h2.title a[href]')
+#
+#
 #for recipe in recipes:
-#    # Now assemble list to pass to requests and beautifulsoup
-#    #links.append(recipe.get_attribute('href'))
-#     for a in recipe.find_all('a', href=True):
-#       print(a['href'])
-#    
-
-recipelinks = []
-recipes = soup.select('h2.title a[href]')
-
-
-for recipe in recipes:
-    recipelinks.append(recipe['href'])
-    #print(recipe['href'])
-
-print(len(recipelinks))
-
+#    recipelinks.append(recipe['href'])
+#    #print(recipe['href'])
+#
+#print(len(recipelinks))
+#
 datalinks = open("recipe_list.txt", "w")
-for link in recipelinks:
-    datalinks.writelines(link+'\n')
+for item in link:
+    datalinks.writelines(item+'\n')
 datalinks.close()
+
+
